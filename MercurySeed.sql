5cfdb3d21c34103e93356df727c70e080b1122d6
Nelson Taylor Lumpkin <ossocasa@gmail.com>
2023-05-17 16:56:57 -0500

    initial commit

---
+++ MercurySeed.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [users] (
+  [id] INT PRIMARY KEY,
+  [firebaseId] NVARCHAR(MAX),
+  [isStaff] BIT,
+  [email] NVARCHAR(255)
+)
+GO
+
+CREATE TABLE [services] (
+  [id] INT PRIMARY KEY,
+  [name] NVARCHAR(255)
+)
+GO
+
+CREATE TABLE [employee] (
+  [id] INT PRIMARY KEY,
+  [userId] INT FOREIGN KEY REFERENCES [users]([id])
+)
+GO
+
+CREATE TABLE [clients] (
+  [id] INT PRIMARY KEY,
+  [userId] INT FOREIGN KEY REFERENCES [users]([id]),
+  [name] NVARCHAR(255),
+  [phone] INT,
+  [hasBC] BIT,
+  [newClient] BIT,
+  [birthday] DATE
+)
+GO
+
+CREATE TABLE [appointment] (
+  [id] INT PRIMARY KEY,
+  [clientId] INT FOREIGN KEY REFERENCES [clients]([id]),
+  [formsId] INT,
+  [serviceId] INT FOREIGN KEY REFERENCES [services]([id]),
+  [preferences] NVARCHAR(MAX),
+  [questions] NVARCHAR(MAX),
+  [familiarity] NVARCHAR(MAX),
+  [time] INT,
+  [isCompleted] BIT
+)
+GO
---
+++ SQLQuery1.sql
@@ -0,0 +1,31 @@
+﻿-- Inserting data into [users] table
+INSERT INTO [users] ([id], [firebaseId], [isStaff], [email]) 
+VALUES 
+(1, 'HAThqBNaPPXYsmwMqRPBHhmLxPz2', 1, 'ossocasa@gmail.com'),
+(2, 'firebaseId2', 1, 'staff2@company.com'),
+(3, 'kSBSRpE2HFdvVYPgZIRxBhaYixR2', 0, 'gmail@gmail.com'),
+(4, 'firebaseId4', 0, 'client2@company.com');
+
+-- Inserting data into [services] table
+INSERT INTO [services] ([id], [name]) 
+VALUES 
+(1, 'Service1'),
+(2, 'Service2');
+
+-- Inserting data into [employee] table
+INSERT INTO [employee] ([id], [userId])
+VALUES 
+(1, 1),
+(2, 2);
+
+-- Inserting data into [clients] table
+INSERT INTO [clients] ([id], [userId], [name], [phone], [hasBC], [newClient], [birthday])
+VALUES 
+(1, 3, 'John Doe', 1234567890, 1, 0, '1985-06-15'),
+(2, 4, 'Jane Doe', 9876543210, 0, 1, '1990-07-20');
+
+-- Inserting data into [appointment] table
+INSERT INTO [appointment] ([id], [clientId], [formsId], [serviceId], [preferences], [questions], [familiarity], [time], [isCompleted])
+VALUES 
+(1, 1, 1, 1, 'Preference1', 'Question1', 'Familiarity1', 1000, 1),
+(2, 2, 2, 2, 'Preference2', 'Question2', 'Familiarity2', 2000, 0);
